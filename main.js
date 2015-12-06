3f2193a443a136f0836fd1f2e92d3ea3d98a8094
Sean M. Snyder <sean.snyder@snhu.edu>
2015-12-05 19:47:18 -0500

    syntax changes

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 var divisible = function () {
-  var getNumber = parseInt(prompt("Up to what number to test divisibility of 3 and 5?")) ;
+  var getNumber = parseInt(prompt("Enter a number to show all divisibilities of 3 and 5?")) ;
   if(getNumber > 30000) {
     $("body").append("<h1 class=\"burn\"> Sorry my friend, you have broken the internets!</h1>");
     alert("Your going to crash the server with a number that high!");
   }else {
   for(var i = 1; i <= getNumber; i++)  {
     if( i % 15 === 0) {
-      $(".wrapper").append("<p class='fifteen' data-percentage=\"15\">" +  i +"<br>divisible by both 3 and 5!</p>");
+      $(".wrapper").append("<p class='fifteen' data-percentage='15'>" +  i +"<br>divisible by both 3 and 5!</p>");
   }else if(i % 5 === 0) {
-    $(".wrapper").append("<p class='five' data-percentage=\"5\">" + i + "<br>divisible by 5 only!</p>");
+    $(".wrapper").append("<p class='five' data-percentage='5'>" + i + "<br>divisible by 5 only!</p>");
   }else if(i % 3 === 0) {
-    $(".wrapper").append("<p class='three' data-percentage=\"3\">" +  i + " <br>divisible by 3 only!</p>");
+    $(".wrapper").append("<p class='three' data-percentage='3'>" +  i + " <br>divisible by 3 only!</p>");
   }else {
-    $(".wrapper").append( "<p class='zero' data-percentage=\"0\">" + i + "</p>" );
+    $(".wrapper").append( "<p class='zero' data-percentage='0'>" + i + "</p>" );
   }
  }
 }
